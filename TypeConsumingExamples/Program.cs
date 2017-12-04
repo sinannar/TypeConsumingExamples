d6ab36d8e8af6176bc7fac7da2672eb8646a8442
Sinan NAR <sinan.nar@gmail.com>
2017-12-04 15:35:51 +1300

    First Commit

---
+++ TypeConsumingExamples/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ 
+    In this project, I re implement the examples that shown on C# certification book
+
+     */
+
+namespace TypeConsumingExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
