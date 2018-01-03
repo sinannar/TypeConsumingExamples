5c2a46deac06f26749dab0110423bd553815786b
Sinan NAR <sinan.nar@gmail.com>
2018-01-03 15:09:21 +1300

    some type consuming examples has been implemented

---
+++ TypeConsumingExamples/Money.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypeConsumingExamples
+{
+    //example of user defined conversions, conversion operator
+    class Money
+    {
+        public Money(decimal amount)
+        {
+            Amount = amount;
+        }
+
+        public decimal Amount { get; set; }
+
+        public static implicit operator decimal(Money money)
+        {
+            return money.Amount;
+        }
+
+        public static implicit operator int(Money money)
+        {
+            return (int)money.Amount;
+        }
+    }
+}
--- TypeConsumingExamples/Program.cs
+++ TypeConsumingExamples/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.IO;
+using System.Net.Http;
 
 /*
  
     In this project, I re implement the examples that shown on C# certification book
 
-     */
+*/
 
 namespace TypeConsumingExamples
 {
@@ -12,7 +14,49 @@ namespace TypeConsumingExamples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("!!! Consume Types !!!");
+
+            //boxing an integer
+            {
+                int i = 42;
+                object o = i;
+                int x = (int)o;
+            }
+
+            //implicitly converting an integer to double
+            {
+                int i = 42;
+                double x = i;
+            }
+
+            //implicitly converting an object to a base type
+            {
+                HttpClient client = new HttpClient();
+                object o = client;
+                IDisposable d = client;
+            }
+
+            //casting double to in
+            {
+                double x = 1234.6;
+                int a;
+                a = (int)x; // a = 1234
+            }
+
+            //explicityl casting a base type to a derived type
+            {
+                Object stream = new MemoryStream();
+                MemoryStream memoryStream = (MemoryStream)stream;
+            }
+
+            //user defined Money class, and casting etc... check the class
+            {
+                Money m = new Money(42.42M);
+                decimal amount = m;
+                int trunctatedAmount = (int)m; // cast is redundent
+            }
+
+            Console.ReadKey();
         }
     }
 }
